048c69343582e4d081394e9420f1f678f2fcb19a
Perparim <perparimshabani.97@gmail.com>
2024-11-14 20:53:39 +0100

    renderer updated

--- src/game/renderer.py
+++ src/game/renderer.py
@@ -1,22 +1,23 @@
 class BoardRenderer:
-    def get_board_html(self, board_state):
+    def get_board_html(self, board_state, guesses):
         html = "<table>"
-        for row in board_state:
+        for i in range(len(board_state)):
             html += "<tr>"
-            for cell in row:
+            for j in range(len(board_state[i])):
+                cell_content = board_state[i][j]
                 cell_class = ""
-                if cell == 'X':
+                if cell_content == 'X':
                     cell_class = "hit"
-                elif cell == 'O':
+                elif cell_content == 'O':
                     cell_class = "miss"
-                html += f'<td class="{cell_class}">{cell if cell != " " else "&nbsp;"}</td>'
+                html += f'<td class="{cell_class}">{cell_content if cell_content != " " else "&nbsp;"}</td>'
             html += "</tr>"
         html += "</table>"
         return html
 
     def get_guess_history_html(self, guess_history):
         html = "<ul>"
-        for move in guess_history:
+        for move in reversed(guess_history[-10:]):  # Show only last 10 moves
             html += f"<li>{move}</li>"
         html += "</ul>"
         return html
